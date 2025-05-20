5eede0d82fd637f83ff359c60ea097765adeccb2
neerajkumar351871 <neerajkumar351871@gmail.com>
2025-05-20 22:58:53 +0530

    calculator

--- Day-5/src/App.jsx
+++ Day-5/src/App.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
-import  styles  from './App.module.css'
-import Calculator from './Calculator'
+import React from 'react';
+
 function App() {
-  console.log(styles.made)
+  const numbers = ['C', 1, 2, 3, '+', 4, 5, 6, '-',  7, 8,9, '/',0];
+
   return (
-   <>
- <Calculator></Calculator>
-   
- 
-   </>
-  )
+    <div className="container d-flex justify-content-center align-items-center min-vh-100">
+      <div className="border rounded shadow p-4" style={{ maxWidth: '400px', width: '100%' }}>
+      <div className="mb-3">
+           <input type="text" className="form-control text-end fs-4" placeholder="0"/>
+        </div>
+        <div className="row g-2">
+          {numbers.map((num, index) =>
+            num !== '' ? (
+              <div key={index} className="col-3">
+                <button type="button" className="btn btn-info w-100 py-3">
+                  {num}
+                </button>
+              </div>
+            ) : (
+              <div key={index} className="col-3"></div>
+            )
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
